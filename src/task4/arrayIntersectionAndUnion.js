2b87999405e6036949984af29e8a270599825780
Marlena Duleba <marlena_duleba@o2.pl>
2024-05-02 22:50:46 +0200

    Drop unnecessary files

--- src/task4/arrayIntersectionAndUnion.js
+++
@@ -1,9 +0,0 @@
-function getArrayIntersection(arr1, arr2) {
-  return arr1.filter((el) => arr2.includes(el));
-}
-
-function getArrayUnion(arr1, arr2) {
-  return [...new Set([...arr1, ...arr2])];
-}
-
-module.exports = { getArrayIntersection, getArrayUnion };
