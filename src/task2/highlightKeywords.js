2b3c0bcfc6d841e57b56c661b37bd6d9954f6320
Marlena Duleba <marlena_duleba@o2.pl>
2024-05-05 09:32:35 +0200

    Add task2 solution

---
+++ src/task2/highlightKeywords.js
@@ -0,0 +1,20 @@
+function highlightKeywords(templateString, arr) {
+  let templateLiteral = "";
+  const strings = templateString.split(/\${\d+}/);
+
+  strings.forEach((str, i) => {
+    templateLiteral +=
+      i in arr ? `${str}<span class='highlight'>${arr[i]}</span>` : `${str}`;
+  });
+
+  return templateLiteral;
+}
+
+const keywords = ["JavaScript", "template", "tagged"];
+const template =
+  "Learn ${0} tagged templates to create custom ${1} literals for ${2} manipulation.";
+
+const highlighted = highlightKeywords(template, keywords);
+
+console.log(highlighted);
+// Expected: "Learn <span class='highlight'>JavaScript</span> tagged templates to create custom <span class='highlight'>template</span> literals for <span class='highlight'>tagged</span> manipulation."
